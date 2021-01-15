201508ed2f32adf8eb00811d6fa88544df4bdb36
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-15 14:22:38 -0300

    On digit method

--- app/src/main/java/com/danieljrodrigues/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/danieljrodrigues/calculatorapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.danieljrodrigues.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onDigit(view: View) {
+        var display = findViewById<TextView>(R.id.tvInput)
+        display.append((view as Button).text)
+    }
+
+    fun onOperator(view: View) {}
+    fun onDecimalPoint(view: View) {}
+    fun onClear(view: View) {}
+    fun onEqual(view: View) {}
 }
